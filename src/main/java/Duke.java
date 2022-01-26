6f075a8748881638740284457c4d94e4b242bd1b
unknown <jschong6567@gmail.com>
2022-01-26 23:34:22 +0800

    Level 1 - Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-______________________-||\n");
+        System.out.println("Hello! I am the most superior AI~~ Friday");
+        System.out.println("What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String echo = "";
+        echo = in.nextLine();
+
+        while(!echo.equals("bye")){
+            System.out.println("=========================================" );
+            System.out.println(echo);
+            echo = in.nextLine();
+        }
+        System.out.println("=========================================" );
+        System.out.println("Bye. Hope to see you again! Love you 3000 <3");
     }
 }

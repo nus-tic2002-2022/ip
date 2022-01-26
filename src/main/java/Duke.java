05c5d295d0c03432b448f1de5a82745b0f454cf3
Jason <wjyeoh1@gmail.com>
2022-01-27 00:08:16 +0800

    Added Level 2 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
 
     public static void greet() {
         String line;
+        String[] list = new String[100];
+
+        int count = 0;
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if (line.toUpperCase().contains("BYE")) {
+            if (line.toLowerCase().contains("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
                 break;
             }
-            System.out.println("\t" + line);
+            if (line.toLowerCase().contains("list")) {
+                for (String item : Arrays.copyOf(list,count)) {
+                    System.out.println("\t"+item);
+                }
+                continue;
+            }
+            System.out.println("\tadded: " + line);
+            list[count] = ++count + ". " + line;
         } while (true);
     }
 

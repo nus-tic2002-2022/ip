6a4ba743219f5ef5779a967cacf3e2c24ff32894
Jason <wjyeoh1@gmail.com>
2022-01-26 21:58:04 +0800

    added Level 1 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void greet() {
+        String line;
+
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        do {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.toUpperCase().contains("BYE")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("\t" + line);
+        } while (true);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke.greet();
     }
 }

06bd3d24abaecdb7f9aff6dc8a327a363d4b3711
Caleb JianHui <jianhuicaleb@gmail.com>
2022-01-29 01:55:46 +0800

    Add functions for introductory message and dividers

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
+    public static void printDivider() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printHeader() {
+        printDivider();
+        String logo = "\t\t\t ____        _        \n"
+                + "\t\t\t|  _ \\ _   _| | _____ \n"
+                + "\t\t\t| | | | | | | |/ / _ \\\n"
+                + "\t\t\t| |_| | |_| |   <  __/\n"
+                + "\t\t\t|____/ \\__,_|_|\\_\\___|\tA variant";
+        System.out.println(logo);
+        printDivider();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Header
+        printHeader();
+        // Introduction Message
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printDivider();
+        System.out.println("Bye. Hope to see you again soon!");
+        printDivider();
     }
 }

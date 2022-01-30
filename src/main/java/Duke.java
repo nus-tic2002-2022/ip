eca514b0d060c0e1ea72106b0cf55ce9cd6e1be8
v_fmkloh <v_fmkloh@proximabeta.com>
2022-01-30 19:55:19 +0800

    Added command|no: to exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
+
+    private static String repeat(char ch, int count) {
+        char[] buf = new char[count];
+        Arrays.fill(buf, ch);
+        return new String(buf);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("I'm Knot YU\n");
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Can I help you?\n");
+
+            line = in.nextLine();
+            if (line.equals("no")) {
+                break;
+            }
+            System.out.println("->\t\t" + line + "\n\n");
+        }
+        var width = 12;
+        System.out.println(repeat('*', width));
+        System.out.println("See YU never");
+        System.out.println(repeat('*', width));
     }
 }

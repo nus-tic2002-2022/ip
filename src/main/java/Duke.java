60fe57c23815328d0802285ea619ef0c0025ca71
keyule <keyule@gmail.com>
2022-01-31 04:53:05 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -15,36 +16,81 @@ public class Duke {
 
         String line;
         Scanner in = new Scanner(System.in);
-        String[] myList;
-        myList = new String[100];
-        int listCount = 0;
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        
 
         boolean start = true;
 
         while (start){
             line = in.nextLine();
-            if(line.equalsIgnoreCase("bye")){
-                System.out.println("---------------------------------");
+            System.out.println("---------------------------------");
+
+            String[] splittedLine = line.split(" ",2);
+            String firstWord = splittedLine[0];
+            firstWord = firstWord.toLowerCase();
+            String restOfLine = "";
+            if(splittedLine.length> 1) {
+                restOfLine = splittedLine[1];
+            }
+
+            if(firstWord.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("---------------------------------");
                 start = false;
-            }else if(line.equalsIgnoreCase("list")){
-                System.out.println("---------------------------------");
-                if(listCount == 0){
-                    System.out.println("nothing to print");
+                in.close();
+            }else if(firstWord.equals("list")){
+                if(Task.getNumberOfTask() == 0){
+                    System.out.println("There is currently nothing in the list");
                 }
-                for(int i = 0; i < listCount; i++){
-                    System.out.println(i+1 + ". " + myList[i]);
+                for(int i = 0; i < taskList.size(); i++){
+                    System.out.println(taskList.get(i));
                 }
-                System.out.println("---------------------------------");
-            }
-            else{
-                myList[listCount] = line;
-                listCount++;
-                System.out.println("---------------------------------");
+            }else if (firstWord.equals("mark")){
+                if(isNumeric(restOfLine)){
+                    int taskNumber = Integer.parseInt(restOfLine);
+                    if(taskNumber > Task.getNumberOfTask() || taskNumber <= 0){
+                        System.out.println("task does not exist");
+                    }else{
+                        if(taskList.get(taskNumber - 1).getDoneStatus()){
+                            System.out.println("task is already done");
+                        }else {
+                            taskList.get(taskNumber - 1).markDone();
+                            System.out.println("marked task as done");
+                        }
+                    }
+                }else{
+                    System.out.println("invalid task number");
+                }
+            }else if(firstWord.equals("unmark")){
+                if(isNumeric(restOfLine)){
+                    int taskNumber = Integer.parseInt(restOfLine);
+                    if(taskNumber > Task.getNumberOfTask() || taskNumber <= 0){
+                        System.out.println("task does not exist");
+                    }else{
+                        if(taskList.get(taskNumber - 1).getDoneStatus()){
+                            taskList.get(taskNumber - 1).unmarkDone();
+                            System.out.println("Unmarked task");
+                        }else {
+                            System.out.println("task is not yet done");
+                        }
+                    }
+                }else{
+                    System.out.println("invalid task number");
+                }
+            }else{
+                Task newTask = new Task(line);
+                taskList.add(newTask);
                 System.out.println("added: " + line);
-                System.out.println("---------------------------------");
             }
+            System.out.println("---------------------------------");
         }
     }
+
+    public static boolean isNumeric(String str) { 
+        try {  
+            Integer.parseInt(str);  
+            return true;
+        } catch(NumberFormatException e){  
+            return false;  
+        }  
+      }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,44 @@
+public class Task {
+    private String description;
+    protected boolean isDone = false;
+    private int id;
+
+    private static int taskCount = 0; 
+
+    public Task(String description) {
+        this.description = description;
+        taskCount++;
+        this.id = taskCount;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    public boolean getDoneStatus(){
+        return this.isDone;
+    }
+
+    public static int getNumberOfTask(){
+        return taskCount;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString(){
+        return id + ".[" + getStatusIcon() + "] " + getDescription();
+    }
+
+
+}

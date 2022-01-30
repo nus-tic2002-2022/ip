36da043511ed6876f202a4c85ab8314f12b9e446
v_fmkloh <v_fmkloh@proximabeta.com>
2022-01-30 20:21:37 +0800

    Added command|list: List all item

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Duke {
-
+    static ArrayList<String> storages = new ArrayList<>();
     private static String repeat(char ch, int count) {
         char[] buf = new char[count];
         Arrays.fill(buf, ch);
@@ -10,17 +11,24 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        System.out.println("I'm Knot YU\n");
+        System.out.println("I'm Knot YU");
         while (true) {
             String line;
             Scanner in = new Scanner(System.in);
-            System.out.print("Can I help you?\n");
+            System.out.print("\nCan I help you?\n");
 
             line = in.nextLine();
             if (line.equals("no")) {
                 break;
             }
-            System.out.println("->\t\t" + line + "\n\n");
+
+            if (line.equals("list")) {
+                for (int i = 0; i < storages.size(); i++)
+                    System.out.println(i+1 + ": " + storages.get(i));
+            } else {
+                storages.add(line);
+                System.out.println("->\t\t+ " + line + "");
+            }
         }
         var width = 12;
         System.out.println(repeat('*', width));

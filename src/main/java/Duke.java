7c63dbb218216e99dcda013cd7a2b4744408aece
LAPTOP-2PQ015U3\AngJingXuan <angjingxuan@yahoo.com.sg>
2022-01-31 12:41:37 +0800

    level-1 w while loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Duke {
 
-    
     public static void main(String[] args) {
 
         String line;
         Scanner in = new Scanner(System.in);
+        int status = 0;
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,16 +15,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println("Hello from\n" + logo);
-        System.out.print("Hello! I'm Duke\n" +
-                "What can I do for you?");
 
-        line = in.nextLine();
+        do {
 
-        if(line.toLowerCase().equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        } else {
-            System.out.println(line);
-        }
+            System.out.print("Hello! I'm Duke\n" +
+                    "What can I do for you?");
+
+            line = in.nextLine();
+
+            if(line.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                status = 1; // can escape
+            } else {
+                System.out.println("added: " + line);
+            }
+            
+        } while (status == 0);
 
     }
 }

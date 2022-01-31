286586ad2bd005601c10b6aadf80b2263353e394
YONG\yong3 <l>
2022-01-31 17:11:42 +0800

    Created introduction to chat bot and reponse type 1 (copy and paste whatever input is given). Exit condition is Bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printIntroduction() {
+        System.out.println("  (\\_/)");
+        System.out.println("  (^_^)");
+        System.out.println("  (____)0");
+        System.out.println("\tHey how's it going? I'm Bugs, a transient robotic bunny.");
+        System.out.println("\tHow may I help? (I take carrots as payment)");
+    }
+    public static void chatting() {
+        printIntroduction();
+
+        Scanner input = new Scanner(System.in);
+        String response;
+
+        while(true) {
+            System.out.println("\tWhat's up doc?");
+            response = input.nextLine();
+            if (response.equals("bye")) {
+                System.out.println("\tAlready? :( Ok then... Cya...");
+                return;
+            }
+            System.out.println("\t" + response);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +32,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        chatting();
+        return;
     }
 }

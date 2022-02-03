684651b22bafa624087c33f0ea7ca41d59cf0d01
Peh_YS <yong362@users.noreply.github.com>
2022-02-03 23:50:44 +0800

    Created sample Level-1 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+       /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("======================");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("======================");
+        while(in.hasNext()) {
+            String reply=in.nextLine();
+            if (reply.equalsIgnoreCase("bye")) {
+                System.out.println("\t================================");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t================================");
+                break;
+            } else {
+                System.out.println("\t==================");
+                System.out.println("\t"+reply);
+                System.out.println("\t==================");
+
+            }
+        }
     }
 }

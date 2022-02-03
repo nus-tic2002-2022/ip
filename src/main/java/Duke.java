4d325cfdf73df7ed59c7be73f9ac62135549533d
Louis Low <louislow@u.nus.edu>
2022-02-03 14:34:21 +0800

    #Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,48 @@ import java.util.Scanner;
 public class Duke {
 
     //Code Starts here!
-    //Level-1
+
+    //Aesthetic Function
     public static void printLine() {
         System.out.println("__________________________________________________________");
     }
 
-    public static void echo() {
+    //Level-2 Added List Array
+    public static void system() {
+        String[] memory = new String[100];
         String line;
+        int counter= 0;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        printLine();
 
-        if (line.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        }
+        //Keep the System Running
+        while (true) {
+            //Ask User for Input
+            line = in.nextLine();
 
-        else {
-            System.out.println(line);
-            printLine();
-            echo();
+            //Print the List
+            if (line.equals("list")) {
+                printLine();
+                for (int x = 0; x < counter; x++) {
+                    System.out.println(x+1 + ". " + memory[x]);
+                }
+                printLine();
+            }
+            //End the System
+            else if (line.equals("bye")) {
+                printLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                System.exit(0);
+            }
+
+            //Add to the List
+            else {
+                memory[counter] = line;
+                printLine();
+                System.out.println("added: " + line);
+                printLine();
+                counter = counter + 1;
+            }
         }
     }
 
@@ -31,9 +54,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        printLine();
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        printLine();
 
         //Trigger Code
-        echo();
+        system();
     }
 }

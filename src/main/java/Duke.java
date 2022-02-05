2b9e781724bd514e105bc35ba52805da331fcf18
Kira <supyaeaung.kira@gmail.com>
2022-02-05 16:42:01 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke
@@ -11,20 +12,38 @@ public class Duke
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("------------------------------------------\n");
-        System.out.println("Hello! I'm Gennie ^_^\n" +
+        tasks();
+    }
+    public static void tasks()
+    {
+        System.out.println("Hello Mater! I'm Gennie ^_^\n" +
                 "     What can I do for you?");
         System.out.println("------------------------------------------\n");
         String line;
+        String[] store = new String [100];
+        int storeWords = 0;
         Scanner in = new Scanner(System.in);
         while (true)
         {
             line = in.nextLine();
             System.out.println("------------------------------------------\n");
-            System.out.println(line);
             if (line.equals("bye") || line.equals("Bye"))
-                System.out.println("Hope to see you again soon!");
+                System.out.println("Summon me anytime if you need me! Bye ~ ");
             if (line.equals("bye")|| line.equals("Bye"))
                 break;
+            if (line.equals("list"))
+            {
+                for (int i = 0; i < store.length; i++)
+                {
+                    if (store[i] != null)
+                    System.out.println((i + 1) + "." + store[i]);
+                }
+                System.out.println("------------------------------------------\n");
+                continue;
+            }
+            store[storeWords++] = line;
+            System.out.println("\tAdded: " + line);
+            System.out.println("------------------------------------------\n");
         }
     }
 }

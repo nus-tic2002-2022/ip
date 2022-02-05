fa006e616cfc2c7f0bb6b7282a0a7fd249708328
Peh_YS <yong362@users.noreply.github.com>
2022-02-06 04:26:31 +0800

    Version Updated - Created Print class    able to print header based on msg count - Decluter of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
+        Print p ;
         String line;
         Scanner in = new Scanner(System.in);
         System.out.println("======================");
@@ -14,17 +15,11 @@ public class Duke {
         System.out.println("======================");
         while(in.hasNext()) {
             String reply=in.nextLine();
+            p = new Print(reply);
             if (reply.equalsIgnoreCase("bye")) {
-                System.out.println("\t================================");
-                System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println("\t================================");
                 break;
-            } else {
-                System.out.println("\t==================");
-                System.out.println("\t"+reply);
-                System.out.println("\t==================");
-
             }
         }
     }
+}
 }
--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Print {
+import javax.sql.rowset.spi.SyncProvider;
+
+public class Print {
+    public Print(){
+
+    }
+    public Print(String msg){
+        String header="";
+        if (msg.equalsIgnoreCase("bye")) {
+            System.out.println("\t================================");
+            System.out.println("\tBye. Hope to see you again soon!");
+            System.out.println("\t================================");
+        } else {
+            for (int i=0; i<msg.length();i++) {
+                header+="=";
+            }
+            System.out.println("\t"+header);
+            System.out.println("\t"+msg);
+            System.out.println("\t"+header);
+
+        }
+    }
 }

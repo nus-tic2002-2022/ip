55a87f3c341445d064e980fe9612bf76bd9ce7ee
LAPTOP-PO0137DO\Daniel <E0648991@u.nus.edu>
2022-02-06 21:43:46 +0800

    version 1(level1 and level2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean isExit = false;
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        String[] text = new String[100];
+        int countNo = 0;
+        while(!isExit){
+            try{
+                String inputData;
+                Scanner s = new Scanner(System.in);
+                inputData = s.nextLine();
+                if(inputData.equals("bye")){
+                    System.out.println("Bye. Hope to see you again soon!");
+                    isExit = true;
+                } else if(inputData.equals("list")){
+                    for(String t: text){
+                        countNo++;
+                        if(t != null){
+                            System.out.println(countNo +". " + t);
+                        }
+                    }
+                } else{
+                    for(int i = 0; i < text.length; i++) {
+                        if (text[i] == null) {
+                            text[i] = inputData;
+                            break;
+                        }
+                    }
+                    System.out.println("added: " +inputData);
+                }
+            } catch(Exception e){
+                System.out.println(e.toString());
+            }
+        }
+
     }
 }

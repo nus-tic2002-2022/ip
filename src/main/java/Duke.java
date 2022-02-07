3d15142ff52e7e343c51162817eea5fc22ccccb5
KHSDESKTOP-97GR\Administrator <jetman51888@gmail.com>
2022-02-07 11:10:44 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,12 +14,27 @@ public class Duke {
 
         String line;
         Scanner in = new Scanner(System.in);
+        String [] word = new String[100];
+        int index = 0;
 
         do{
             line = in.nextLine();
             printWithLine(List.of());
             printWithLine(List.of(line));
-
+            if (line.equals("list")){
+                printWithLine(List.of());
+                int k = 1;
+                for (int i = 0; i < index; i++){
+                    System.out.println("     " + k + ". " + word[i]);
+                    k++;
+                }
+                printWithLine(List.of());
+            }else if (!line.equals("bye")){
+                word[index] = line;
+                index++;
+                printWithLine(List.of());
+                printWithLine(List.of("added: " + line));
+            }
         }while(!line.equals("bye"));
 
         printWithLine(List.of("Bye. Hope to see you again soon!"));

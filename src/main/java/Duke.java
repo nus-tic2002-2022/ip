9d1ff4192abfa301a19d17acb70ef0e4c8d75e12
KHSDESKTOP-97GR\Administrator <jetman51888@gmail.com>
2022-02-09 14:55:38 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,47 +10,52 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
+        System.out.println("______________________________________________");
         printWithLine(List.of("Hello! I'm Duke", "What can I do for you?"));
 
         String line;
         Scanner in = new Scanner(System.in);
-        String [] word = new String[100];
-        int index = 0;
+        List<Task> item = new ArrayList<>();
 
-        do{
+        do {
             line = in.nextLine();
-            printWithLine(List.of());
-            printWithLine(List.of(line));
-            if (line.equals("list")){
+
+            if (line.equals("list")) {
                 printWithLine(List.of());
-                int k = 1;
-                for (int i = 0; i < index; i++){
-                    System.out.println("     " + k + ". " + word[i]);
-                    k++;
+                List<String> messages = new ArrayList<>();
+                System.out.println("   Here are the tasks in your list: ");
+                for (int i = 0; i < item.size(); i++) {
+                    messages.add(i + 1 + "." + item.get(i));
                 }
+                printWithLine(messages);
+            } else if (line.split(" ")[0].equals("done")) {
                 printWithLine(List.of());
-            }else if (!line.equals("bye")){
-                word[index] = line;
-                index++;
+                Task markItem = item.get(Integer.parseInt(line.substring(5)) - 1);
+                markItem.markAsDone();
+                printWithLine(List.of("Nice! I've marked this task as done: ", " " + markItem));
+
+
+            } else if (!line.equals("bye")) {
+                Task task = new Task(line);
+                item.add(task);
                 printWithLine(List.of());
-                printWithLine(List.of("added: " + line));
+                printWithLine(List.of("added: " + task));
             }
-        }while(!line.equals("bye"));
-
-        printWithLine(List.of("Bye. Hope to see you again soon!"));
-
-    }
-
-    private static void printWithLine(List<String> messages){
-        for (String message : messages){
-            System.out.println("   " + message);
         }
-        System.out.println("______________________________________________");
+        while (!line.equals("bye"));
+        printWithLine(List.of());
+        printWithLine(List.of("Bye. Hope to see you again soon!"));
     }
 
+            private static void printWithLine (List < String > messages) {
+                for (String message : messages) {
+                    System.out.println("   " + message);
+                }
+                System.out.println("______________________________________________");
+            }
 
-}
 
+
+    }
 
 

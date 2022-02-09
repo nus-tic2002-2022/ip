7e7c3b3df3047a7aebbbb483b1d2e5198c6dcdc0
AjazYeap <98638985+AjazYeap@users.noreply.github.com>
2022-02-10 00:32:07 +0800

    Add files via upload
    Added level 1

---
+++ src/main/java/echo.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class echo {
+    private static ArrayList <String> buffer = new ArrayList<>();
+    public static void greet ()
+    {
+        Scanner scanInput = new Scanner(System.in);
+        String passed = scanInput.nextLine();
+        //buffer.add(passed);
+        //System.out.println(passed);
+        int c_flag =0;
+        for (int i =0; !passed.equals("bye");)
+        {
+            if(passed.isEmpty()) //if empty, just skip
+            {
+                System.out.println("Size of buffer: " + buffer.size());
+                passed = scanInput.nextLine();
+                continue;
+            }
+            if(buffer.contains(passed)) //if contains, print out but no need add into buffer
+            {
+                c_flag = 1;
+                System.out.println ("added: " + passed);
+                System.out.println("Size of buffer: " + buffer.size());
+                passed = scanInput.nextLine();
+                c_flag=0;
+                continue;
+            }
+            if(c_flag == 0) // if not contains, print and add into buffer
+            {
+                System.out.println(passed);
+                buffer.add(passed);
+                System.out.println("Size of buffer: " + buffer.size());
+                passed = scanInput.nextLine();
+                continue;
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}

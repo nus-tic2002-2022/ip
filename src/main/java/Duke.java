d677e44d7d38a651ee1b83f9bca47cbac4b2d24f
Jia Jie <45411305+JiaJieRion@users.noreply.github.com>
2022-02-10 23:38:11 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,54 @@
 import java.util.Scanner;
 
+
 public class Duke {
+
+    //Level 1 Greet, Echo, Exit
+    public static void bye(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0); //Exit program
+    }
+
+    //Level 2 Add, List
+    public static void list(Task[] taskList, int numOfTasks){
+        System.out.println("Here are the tasks in your list:\n");
+        for (int i = 0; i < numOfTasks; i++) {
+            System.out.println(i + 1 + "." + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
+        }
+    }
+
+    //Level 3 Mark as Done
+    public static boolean mark(String userInput) {
+        boolean isDone = false;
+        String mark = userInput.substring(0, userInput.indexOf(' ')).trim();
+        if (mark.equals("mark")) {
+            System.out.println("Nice! I've marked this task as done:\n");
+            isDone = true;
+        }else if (mark.equals("unmark")) {
+            System.out.println("OK, I've marked this task as not done yet:\n");
+        }
+        return isDone;
+    }
+
+    //Finding index of taskList
+    public static int index(String userInput, int numOfTasks){
+        if(numOfTasks == 0){
+            System.out.println("There is no task in the list!");
+            return 0;
+        }
+        String listIndexer = userInput;
+        listIndexer = listIndexer.replaceAll("[^\\d]","");
+        int index = Integer.parseInt(listIndexer);
+        if(index > numOfTasks){
+            System.out.println("ERROR: The number entered exceeds the number of task(s) in the list!\n");
+            System.out.println( numOfTasks + " task(s) in the list.\n");
+            return 0;
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,30 +56,41 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
 
-        String[] userInputList = new String[100];
-        int numOfInputs = 0;
+        Task[] taskList = new Task[100];
+        int numOfTasks = 0;
 
-    while(true) {
-        Scanner scanInput = new Scanner(System.in); //Scan user input
-        String userInput = scanInput.nextLine(); //Read user input
+        while(true) {
+            Scanner scanInput = new Scanner(System.in); //Scan user input
+            String userInput = scanInput.nextLine(); //Read user input
 
-        //Level 1 Greet, Echo, Exit
-        if(userInput.equals("bye")){
-            System.out.println("Bye. Hope to see you again soon!");
-            System.exit(0); //Exit program
-        }
-
-        //Level 2 Add, List
-        if(userInput.equals("list")) {
-            for (int i = 0; i < numOfInputs; i++) {
-                System.out.println(i + 1 + ". " + userInputList[i]);
+            //Level 1
+            if(userInput.equals("bye")){
+                Duke.bye();
             }
-            continue;
-        }
-        userInputList[numOfInputs] = userInput;
-        numOfInputs++;
 
-        System.out.println("added: " + userInput); //Echo
-    }}
+            //Level 2
+            if(userInput.equals("list")){
+                Duke.list(taskList, numOfTasks);
+                continue;
+            }
+
+            //Level 3
+            if(userInput.contains("mark")) {
+                int index = Duke.index(userInput, numOfTasks) - 1;
+                if(index == -1){
+                    continue;
+                }
+                taskList[index].isDone = Duke.mark(userInput);
+                System.out.println(" [" + taskList[index].getStatusIcon() + "] " + taskList[index].description);
+                continue;
+            }
+
+            //Adding tasks
+            Task t = new Task(userInput);
+            taskList[numOfTasks] = t;
+            numOfTasks++;
+            System.out.println("added: " + userInput); //Echo / add task
+        }
+    }
 
 }

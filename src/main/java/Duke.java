5f7a6bc4e96eaac76a12feffcf2b1793a0c7c27d
hambot072 <97346059+hambot072@users.noreply.github.com>
2022-02-10 23:56:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,16 +11,33 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         String line = "";
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        String[] temp = new String[100];
+        int itemCount = 0;
+        int listCount = 1;
 
         while(!line.contains("bye")) {
             System.out.print("Type something: ");
             line = in.nextLine();
-            System.out.println("\tYou said: " + line);
 
             if (line.equals("bye")){
                 System.out.print("\tGood day and good bye");
                 break;
             }
+
+            if(line.equals("list")) {
+                list = Arrays.copyOf(temp, itemCount);
+                for (int i = 0; i < list.length; i++) {
+                    System.out.print(listCount + ". " + list[i] + "\n");
+                    listCount++;
+                }
+            }
+            else {
+                temp[itemCount] = line;
+                itemCount++;
+                System.out.println("\tadded: " + line);
+            }
+
         }
     }
 }

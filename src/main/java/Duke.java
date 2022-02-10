abb935959f7c036e91b4db48f5abdc218ca5566c
hambot072 <97346059+hambot072@users.noreply.github.com>
2022-02-11 01:21:50 +0800

    Level-3
    Added Task.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.regex.*;
 
 public class Duke {
     public static void main(String[] args) {
+        Pattern p = Pattern.compile("^mark[\\s][0-9]*$");
+        Pattern q = Pattern.compile("^unmark[\\s][0-9]*$");
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,14 +15,16 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         String line = "";
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
-        String[] temp = new String[100];
+        Task[] list = new Task[100];
+        Task[] temp = new Task[100];
         int itemCount = 0;
-        int listCount = 1;
+
 
         while(!line.contains("bye")) {
             System.out.print("Type something: ");
             line = in.nextLine();
+            Matcher m = p.matcher(line);
+            Matcher n = q.matcher(line);
 
             if (line.equals("bye")){
                 System.out.print("\tGood day and good bye");
@@ -28,16 +34,30 @@ public class Duke {
             if(line.equals("list")) {
                 list = Arrays.copyOf(temp, itemCount);
                 for (int i = 0; i < list.length; i++) {
-                    System.out.print(listCount + ". " + list[i] + "\n");
-                    listCount++;
+                    System.out.print(i+1 + "." + list[i].getStatusIcon() + list[i].getDescription() + "\n");
                 }
             }
             else {
-                temp[itemCount] = line;
-                itemCount++;
-                System.out.println("\tadded: " + line);
-            }
 
+                if(m.matches()){
+                    line = line.replaceAll("[^0-9]", "");
+                    list[Integer.parseInt(line)-1].setStatus(true);
+                    System.out.println("This task has been marked as done: " + "\n" + list[Integer.parseInt(line)-1].getStatusIcon() + list[Integer.parseInt(line)-1].getDescription());
+                }
+                else if(n.matches()){
+                    line = line.replaceAll("[^0-9]", "");
+                    list[Integer.parseInt(line)-1].setStatus(false);
+                    System.out.println("This task has been marked as not done: " + "\n" + list[Integer.parseInt(line)-1].getStatusIcon() + list[Integer.parseInt(line)-1].getDescription());
+                }
+                else {
+                    Task t = new Task(line);
+                    temp[itemCount] = t;
+                    itemCount++;
+                    System.out.println("\tadded: " + line);
+                }
+
+            }
         }
     }
 }
+

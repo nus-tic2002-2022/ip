51f8136acbbc214c738a12ff98b2bc64492d4a09
Jia Jie <45411305+JiaJieRion@users.noreply.github.com>
2022-02-10 20:48:52 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,16 +9,30 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
 
+        String[] userInputList = new String[100];
+        int numOfInputs = 0;
+
     while(true) {
         Scanner scanInput = new Scanner(System.in); //Scan user input
         String userInput = scanInput.nextLine(); //Read user input
 
         //Level 1 Greet, Echo, Exit
         if(userInput.equals("bye")){
-            System.out.print("Bye. Hope to see you again soon!");
+            System.out.println("Bye. Hope to see you again soon!");
             System.exit(0); //Exit program
         }
-        System.out.println(userInput); //Echo
+
+        //Level 2 Add, List
+        if(userInput.equals("list")) {
+            for (int i = 0; i < numOfInputs; i++) {
+                System.out.println(i + 1 + ". " + userInputList[i]);
+            }
+            continue;
+        }
+        userInputList[numOfInputs] = userInput;
+        numOfInputs++;
+
+        System.out.println("added: " + userInput); //Echo
     }}
 
 }

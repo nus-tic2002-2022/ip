5116f1b2e07ec0a6f1825b547f94a0aec61550da
hambot072 <97346059+hambot072@users.noreply.github.com>
2022-02-10 23:22:15 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String line = "";
+        Scanner in = new Scanner(System.in);
+
+        while(!line.contains("bye")) {
+            System.out.print("Type something: ");
+            line = in.nextLine();
+            System.out.println("\tYou said: " + line);
+
+            if (line.equals("bye")){
+                System.out.print("\tGood day and good bye");
+                break;
+            }
+        }
     }
 }

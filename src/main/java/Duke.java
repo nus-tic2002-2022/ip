12fca8636163d8c69b3f4c48b5055c0590e96c4b
LAPTOP-TCR1IDUO\Kevin Xu Xin <xxydhcg@hotmail.com>
2022-02-12 19:20:26 +0800

    Level-2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,21 +9,54 @@ public class Duke {
 
     public static void system() {
 
+        String[] taskList = new String[100];
         String line;
-
+        int counter= 0;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        separatingLine();
 
-        if (line.equals("bye")) {
-            System.out.println("Bye. Glad to be of service to you!\n" +
-                    "Hope to assist you again soon!");
-        }
 
-        else {
-            System.out.println(line);
-            separatingLine();
-            system();
+        while (true) {
+
+            //request user to input:
+            line = in.nextLine();
+
+            if (line.equals("bye")) {
+                separatingLine();
+                System.out.println("Bye. Glad to be of service to you!\n" +
+                        "Hope to assist you again soon!");
+                separatingLine();
+                System.exit(0);
+            }
+
+            //Print the List:
+            if (line.equals("list") && taskList[0] != null) {
+                separatingLine();
+                System.out.println("Hey, I have summarised your task list as below:");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i + 1 + ". " +
+                            taskList[i]);
+                }
+                separatingLine();
+            } else if (line.equals("list") && taskList[0] == null) {
+                separatingLine();
+                System.out.println("Your current task list is empty!\n" +
+                        "Please start to add your tasks below ^<>^");
+                separatingLine();
+            }
+
+            //Add user input to task list:
+            else {
+                separatingLine();
+                System.out.println("The below task has been added to your task list:\n " +
+                        line);
+
+                //assign to task list from input:
+                taskList[counter] = line;
+
+                separatingLine();
+                counter = counter + 1;
+            }
+
         }
 
     }
@@ -38,12 +71,13 @@ public class Duke {
         separatingLine();
         System.out.println("Hello! This is Duke here!\n" +
                 "Your trusted personal task assistant!\n" +
-                "What can I do to assist you today?");
+                "What can I do to assist you today?\n" +
+                "(Please input your task or type 'list' to retrieve your task list)");
         separatingLine();
 
         // perform system logic
         system();
 
     }
-    
+
 }

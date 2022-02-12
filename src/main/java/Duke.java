04d6cc9952b5294e7241a44ca1575fe82085d804
Peh_YS <yong362@users.noreply.github.com>
2022-02-12 11:15:51 +0800

    Level 2 - Add and List Function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
-       /* String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("======================");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("======================");
-        while(in.hasNext()) {
-            String reply=in.nextLine();
+public static void main(String[] args) {
+   /* String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    System.out.println("Hello from\n" + logo);*/
+    List<Task> TL = new ArrayList<>();
+    Scanner in = new Scanner(System.in);
+    System.out.println("======================");
+    System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    System.out.println("======================");
+    while(in.hasNext()) {
+        String reply=in.nextLine();
+        if (reply.equalsIgnoreCase("bye")){
             new Print(reply);
-            if (reply.equalsIgnoreCase("bye"))
-                break;
+            break;
+        }
+        else if (reply.equalsIgnoreCase("list")){
+            new Print((ArrayList<Task>) TL);
+        }else {
+            new Print(reply);
+            TL.add(new Todo(reply));
         }
     }
+}
+
+
 }
 
--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Print {
-    public Print(){
+    String divider="==========================================";
+    public Print() {
 
     }
-    public Print(String msg){
-        String header="";
+
+    public Print(String msg) {
+
+
         if (msg.equalsIgnoreCase("bye")) {
             System.out.println("\t================================");
             System.out.println("\tBye. Hope to see you again soon!");
             System.out.println("\t================================");
         } else {
-            for (int i=0; i<msg.length();i++) {
-                header+="=";
-            }
-            System.out.println("\t"+header);
-            System.out.println("\t"+msg);
-            System.out.println("\t"+header);
-
+            System.out.println("\t" + divider);
+            System.out.println("\tadded:" + msg);
+            System.out.println("\t" + divider);
         }
     }
+    public Print(ArrayList<Task> TL){
+        System.out.println(divider);
+        for (int i =0; i<TL.size();i++){
+            System.out.println((i+1)+". "+TL.get(i).description);
+        }
+        System.out.println(divider);
+    }
+
 }
+

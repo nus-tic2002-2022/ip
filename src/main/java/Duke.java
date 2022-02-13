a87fb76591487bf4c1999fe57656d4d11dfa4015
unknown <yeojy9@hotmail.com>
2022-02-13 21:42:42 +0800

    PR

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,9 +14,25 @@ public class Duke {
         String str;
         str = in.nextLine();
 
+        String [] list = new String [100];
+        int count = 0;
+
+
         while (!str.equals("bye")){
+            if(str.equals("list"))
+            {
+                for (int i = 0; i<count ; i++ ){
+                    System.out.println(i+1 + ". " + list[i]);
+                }
+                
+            }
+            else{
+                System.out.println("added: " + str);
+                list[count] = str;
+                count++;
+            }
                 System.out.println("________________________________");
-                System.out.println(str);
+                //System.out.println(str);
                 str = in.nextLine();
         }
         System.out.println("________________________________\n");

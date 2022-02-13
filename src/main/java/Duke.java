a3e16fe658e6dce345c14d6632bf067dc246a902
repulse1 <zhixuan_04@hotmail.com>
2022-02-13 19:38:35 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("I'm Duke\n" + "What can I do for you?");
+
+        String input;
+        Scanner in = new Scanner(System.in);
+        boolean bye = false;
+
+
+
+        while (bye == false) {
+
+            input = in.nextLine();
+
+            if(input.toLowerCase().equals("bye"))
+            {
+                System.out.println("Bye bye!");
+                bye = true;
+            }
+            else {
+                System.out.println(input);
+            }
+        }
     }
 }

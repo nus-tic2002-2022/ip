e2829f3fe103a54d80f53c6f27b5dc343d37d5b8
repulse1 <zhixuan_04@hotmail.com>
2022-02-15 01:02:47 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,45 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String[] list = new String[100];
-    private static int listCount = 0;
+    private static Task[] taskList = new Task[100];
+    private static int taskCount = 0;
 
-    public static void addItem(String item) {
-        list[listCount] = item;
-        listCount++;
+    public static void addItem(Task item) {
+        taskList[taskCount] = item;
+        taskCount++;
+    }
+
+    public static void bye() {
+        System.out.println("Bye bye!");
+        System.exit(0);
+    }
+
+    private static void list(Task[] taskList, int taskCount) {
+        if (taskCount > 0) {
+            System.out.println("Here are the tasks in your list: ");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(i + 1 + "." + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
+            }
+        }
+        else {
+            System.out.println("No tasks in your list.");
+        }
+    }
+
+    public static boolean mark(String input) {
+
+        boolean isDone = false;
+
+        String markTask = input.substring(0, input.indexOf(' ')).trim();
+        if (markTask.equals("mark")) {
+            System.out.println("Nice! I've marked this task as done: ");
+            isDone = true;
+        }
+        else if (markTask.equals("unmark")) {
+            System.out.println("OK, I've marked this task as not done yet: ");
+        }
+
+        return isDone;
     }
 
     public static void main(String[] args) {
@@ -22,24 +55,34 @@ public class Duke {
 
         String input;
         Scanner in = new Scanner(System.in);
-        boolean bye = false;
 
-        while (bye == false) {
+        while (true) {
             input = in.nextLine();
 
             if (input.toLowerCase().equals("bye")) {
-                System.out.println("Bye bye!");
-                bye = true;
+                bye();
             }
+
             if (input.toLowerCase().equals("list")) {
-                for (int i = 0; i < listCount; i++){
-                    System.out.println(list[i]);
+                list(taskList, taskCount);
+                continue;
+            }
+
+            if(input.contains("mark")) {
+                String index = input.replaceAll("\\D+","");
+                int taskIndex = Integer.parseInt(index)-1;
+                if(taskIndex == -1){
+                    continue;
                 }
+                taskList[taskIndex].isDone = mark(input);
+                System.out.println(" [" + taskList[taskIndex].getStatusIcon() + "] " + taskList[taskIndex].description);
+
+                continue;
             }
-            else {
-                addItem(input);
-                System.out.println("added: " + input);
-            }
+
+            Task t = new Task(input);
+            addItem(t);
+            System.out.println("added: " + input);
         }
     }
 }

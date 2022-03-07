450b318b82fa3ff715099125f25c3caf43ef9fa0
YONG\yong3 <l>
2022-03-07 22:15:48 +0800

    Too many changes here...

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,46 +1,54 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Duke {
 
     //enum for the tasks
 
-    private static Task[] tasks = new Task[100];
+    private static ArrayList<Task> tasks = new ArrayList<Task>();
     private static int taskListCount = 0;
 
     public static void printList() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0 ; i < taskListCount ; i ++) {
-            System.out.println( (i+1)+"."+tasks[i] ); //+1 to i here due to numbering
+            System.out.println( (i+1)+"."+tasks.get(i) ); //+1 to i here due to numbering
         }
     }
     
     public static void changeTaskStatus(int taskIndex, boolean statusToChange) {
-        tasks[taskIndex].setDone(statusToChange);
-        System.out.printf(tasks[taskIndex].getTask() + " set to %s.\n",statusToChange ? "done" : "undone");
+        tasks.get(taskIndex).setDone(statusToChange);
+        System.out.printf(tasks.get(taskIndex).getTask() + " set to %s.\n",statusToChange ? "done" : "undone");
     }
 
-    public static void processTaskToList(String taskType, String response) {
-
-        String task = response.substring(response.indexOf(' ') + 1); //can be improved
-        System.out.println(task);
-
+    public static void processTaskToList(String taskType, String response) throws ArrayIndexOutOfBoundsException {
         if (taskType.equals("todo")) {
-            tasks[taskListCount] = new ToDos(task);
-        }
-
-        if (taskType.equals("deadline")) {
+            String task = response.split(" ")[1];
+            tasks.add(new ToDos(task));
+        } else if (taskType.equals("deadline")) {
+            //throw error (what if wrong format)?
+            String task = response.substring(response.indexOf(' ') + 1); //can be improved
             String[] taskDetails = task.split(" /by ");
-            tasks[taskListCount] = new Deadlines(taskDetails[0],taskDetails[1]);
+            tasks.add(new Deadlines(taskDetails[0],taskDetails[1]));
+            //tasks[taskListCount] = new Deadlines(taskDetails[0],taskDetails[1]);
+        } else if (taskType.equals("event")) {
+            //throw error (what if wrong format)?
+            String task = response.substring(response.indexOf(' ') + 1); //can be improved
+            String[] taskDetails = task.split(" /at ");
+            tasks.add(new Events(taskDetails[0],taskDetails[1]));
+            //tasks[taskListCount] = new Events(taskDetails[0],taskDetails[1]);
         }
 
-        if (taskType.equals("event")) {
-            String[] taskDetails = task.split(" /at ");
-            tasks[taskListCount] = new Events(taskDetails[0],taskDetails[1]);
-        }
         System.out.println("Added the following task to your list:");
-        System.out.println(tasks[taskListCount]);
+        System.out.println(tasks.get(taskListCount));
         taskListCount ++;
+        System.out.printf("You now have %d in your list!\n", taskListCount);
+    }
+
+    public static void deleteTasks (String response) {
+        int deleteIndex = Integer.parseInt(response.split(" ")[1]) - 1; //what if it's not int?
+        System.out.printf("Okay! I've deleted the task '%s' from your list!\n", tasks.get(deleteIndex).getTask());
+        tasks.remove(deleteIndex);
+        taskListCount --;
         System.out.printf("You now have %d in your list!\n", taskListCount);
     }
 
@@ -54,32 +62,33 @@ public class Duke {
             System.out.println("\tWhat's up doc?");
             response = input.nextLine();
 
-            String firstWord = response.split(" ")[0];
-
-            if (firstWord.equals("bye")) {
+            if (response.equals("bye")) {
                 printBye();
                 return;
-            }
-
-            if (firstWord.equals("list")) {
+            } else if (response.equals("list")) {
                 printList();
                 continue;
-            }
-
-            if ( firstWord.equals("mark") ) {
+            } else if ( response.startsWith("mark") ) {
+                //error here. what if mark is not integer?
                 int indexToChange = Integer.parseInt(response.split(" ")[1]) - 1 ; //minus 1 to account for Java starting array at index 0
                 changeTaskStatus(indexToChange,true);
                 continue;
-            }
-
-            if ( firstWord.equals("unmark")) {
+            } else if ( response.startsWith("unmark")) {
+                //error here. what if unmark is not integer?
                 int indexToChange = Integer.parseInt(response.split(" ")[1]) - 1; //minus 1 to account for Java starting array at index 0
                 changeTaskStatus(indexToChange,false);
                 continue;
-            }
-
-            if (firstWord.matches("todo|event|deadline")) {
-                processTaskToList(firstWord,response);
+            } else if (response.startsWith("todo") || response.startsWith("event") || response.startsWith("deadline")) {
+                String taskType = response.split(" ")[0];
+                try {
+                    processTaskToList(taskType,response);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("=^-.-^= You didn't provide your task description...");
+                }
+            } else if (response.startsWith("delete")) {
+                deleteTasks(response);
+            } else {
+                System.out.println("=^-.-^= Not sure what that means...");
             }
 
             //tasks[taskListCount] = new Task(response);

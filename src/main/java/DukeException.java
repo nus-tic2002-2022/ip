ab1b817e28a2efbf68b42dafeda63387cba1b301
Kira <supyaeaung.kira@gmail.com>
2022-03-08 23:49:42 +0800

    Level 5 (pending to edit)

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,8 +1,13 @@
 public class DukeException extends Exception
 {
+    private String error;
+
     public DukeException (String error)
     {
         super(error);
+    }
+    public String getError() {
+        return this.error;
     }
 
 }

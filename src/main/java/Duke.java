d31d639db369d012b6de122098c3704a93594bf9
YONG\yong3 <l>
2022-03-16 22:56:31 +0800

    Remove the UI-related functions to reduce code bulk

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -97,28 +97,15 @@ public class Duke {
     }
 
     public static void printIntroduction() {
-        System.out.println("  (\\_/)");
-        System.out.println("  (^_^)");
-        System.out.println("  (____)0");
-        System.out.println("\tHey how's it going? I'm Bugs, a transient robotic bunny.");
-        System.out.println("\tHow may I help? (I take carrots as payment)");
+        ui;
     }
 
     public static void printBye() {
-        System.out.println("Already? Cya...");
-        System.out.println("  //");
-        System.out.println(" ('')");
-        System.out.println(" UU \\   ");
-        System.out.println(" <><>*");
+        ui;
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        ui;
         chatting();
         return;
     }

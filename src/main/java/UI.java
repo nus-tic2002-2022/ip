21550e65c1c896a7e55df50fd6ae4217728b6650
YONG\yong3 <l>
2022-03-16 21:40:13 +0800

    Added intro bye, and logo texts to UI methods

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,14 +1,30 @@
-package main.java;
-
-
 public class UI
 {
-    public void welcome () {
+    public UI () { };
 
-    };
+    public static void printIntroduction() {
+        System.out.println("  (\\_/)");
+        System.out.println("  (^_^)");
+        System.out.println("  (____)0");
+        System.out.println("\tHey how's it going? I'm Bugs, a transient robotic bunny.");
+        System.out.println("\tHow may I help? (I take carrots as payment)");
+    }
 
-    public void bye () {
-    };
+    public static void printBye() {
+        System.out.println("Already? Cya...");
+        System.out.println("  //");
+        System.out.println(" ('')");
+        System.out.println(" UU \\   ");
+        System.out.println(" <><>*");
+    }
 
-    public void
+    public static void printLogo() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
 }

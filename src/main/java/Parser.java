be14f50f25623a7498feb343a4b490c3d3613f82
YONG\yong3 <l>
2022-03-19 00:20:09 +0800

    Removed 'continue' in parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,30 +2,27 @@ public class Parser {
 
     public static void processUserInput(String userInput) {
         if (userInput.equals("bye")) {
-            UI.printBye();
+            ui.printBye();
             return;
         } else if (userInput.equals("list")) {
-            printList();
-            continue;
+            //ui.printListUI();
         } else if ( userInput.startsWith("mark") ) {
             //error here. what if mark is not integer?
             int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1 ; //minus 1 to account for Java starting array at index 0
-            changeTaskStatus(indexToChange,true);
-            continue;
+            TaskProcessor.changeTaskStatus(indexToChange,true);
         } else if ( userInput.startsWith("unmark")) {
             //error here. what if unmark is not integer?
             int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1; //minus 1 to account for Java starting array at index 0
-            changeTaskStatus(indexToChange,false);
-            continue;
+            TaskProcessor.changeTaskStatus(indexToChange,false);
         } else if (userInput.startsWith("todo") || userInput.startsWith("event") || userInput.startsWith("deadline")) {
             String taskType = userInput.split(" ")[0];
             try {
-                processTaskToList(taskType,userInput);
+                TaskProcessor.processTaskToList(taskType,userInput);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("=^-.-^= You didn't provide your task description...");
             }
         } else if (userInput.startsWith("delete")) {
-            deleteTasks(userInput);
+            TaskProcessor.deleteTasks(userInput);
         } else {
             System.out.println("=^-.-^= Not sure what that means...");
         }

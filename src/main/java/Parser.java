3adde52c8f5f77aea5e1f5d4e5928523cbc1a667
YONG\yong3 <l>
2022-03-20 19:36:14 +0800

    added error catching for marking and unmarking of tasks: when mark is out of bounds, or when mark value x is not an integer

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
+
 public class Parser {
 
-    public static void processUserInput(String userInput) {
+    public static void processUserInput(String userInput, ArrayList<Task> tasks , int taskListCount) { //
         if (userInput.equals("list")) {
-            //ui.printListUI();
+            TaskProcessor.printList();
         } else if ( userInput.startsWith("mark") ) {
-            //error here. what if mark is not integer?
-            int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1 ; //minus 1 to account for Java starting array at index 0
-            TaskProcessor.changeTaskStatus(indexToChange,true);
+            //error here. what if mark is not integer
+            try {
+                int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1 ;
+                TaskProcessor.changeTaskStatus(indexToChange,true);
+            } catch (NumberFormatException e) {
+                System.out.println("Task number not specified properly..");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("I don't think you have that many tasks on your list...");
+            };
+
         } else if ( userInput.startsWith("unmark")) {
-            //error here. what if unmark is not integer?
-            int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1; //minus 1 to account for Java starting array at index 0
-            TaskProcessor.changeTaskStatus(indexToChange,false);
-        } else if (userInput.startsWith("todo") || userInput.startsWith("event") || userInput.startsWith("deadline")) {
+            try {
+                int indexToChange = Integer.parseInt(userInput.split(" ")[1]) - 1 ;
+                TaskProcessor.changeTaskStatus(indexToChange,false);
+            } catch (NumberFormatException e) {
+                System.out.println("Task number not specified properly..");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("I don't think you have that many tasks on your list...");
+            };
+        } else if (userInput.startsWith("todo " ) || userInput.startsWith("event ") || userInput.startsWith("deadline ")) {
             String taskType = userInput.split(" ")[0];
             try {
                 TaskProcessor.processTaskToList(taskType,userInput);

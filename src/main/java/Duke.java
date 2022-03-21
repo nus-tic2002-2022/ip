dce06f596460009be7bc6706fd580bd010ebc104
LAPTOP-O4H9CQBN\zhixu <zhixuan_04@hotmail.com>
2022-03-21 19:14:51 +0800

    level-6 - error handling for non-integer - converted to arrayList - delete task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,19 +5,18 @@ import exceptions.UnknownCommandException;
 
 public class Duke {
 
-    private static Task[] taskList = new Task[100];
-    private static int taskCount = 0;
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void bye() {
         System.out.println("Bye bye!");
         System.exit(0);
     }
 
-    private static void list(Task[] taskList, int taskCount) {
-        if (taskCount > 0) {
+    private static void list() {
+        if (!taskList.isEmpty()) {
             System.out.println("Here are the tasks in your list: ");
-            for (int i = 0; i < taskCount; i++) {
-                System.out.println(i + 1 + "." + taskList[i]);
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println(i + 1 + "." + taskList.get(i));
             }
         }
         else {
@@ -30,29 +29,28 @@ public class Duke {
         String markTask = input.substring(0, input.indexOf(' ')).trim();
         boolean success;
         try {
-            if (taskIndex<1) throw new UnknownCommandException("List is empty.");
-            if (taskCount<taskIndex) throw new UnknownCommandException("Selected task to mark is invalid.");
-            taskIndex = taskIndex-1;
+            if (taskIndex<1 || taskList.size()<taskIndex) throw new UnknownCommandException("Selected task to mark is invalid.");
+            taskIndex--;
             if (markTask.equals("mark")) {
-                success = taskList[taskIndex].markAsDone();
+                success = taskList.get(taskIndex).markAsDone();
                 if (success) {
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(taskList[taskIndex]);
+                    System.out.println(taskList.get(taskIndex));
                 }
                 else {
                     System.out.println("Task is already marked as done: ");
-                    System.out.println(taskList[taskIndex]);
+                    System.out.println(taskList.get(taskIndex));
                 }
             }
             else if (markTask.equals("unmark")) {
-                success = taskList[taskIndex].markAsUndone();
+                success = taskList.get(taskIndex).markAsUndone();
                 if (success) {
                     System.out.println("OK, I've marked this task as not done yet: ");
-                    System.out.println(taskList[taskIndex]);
+                    System.out.println(taskList.get(taskIndex));
                 }
                 else {
                     System.out.println("Task is already marked as not done: ");
-                    System.out.println(taskList[taskIndex]);
+                    System.out.println(taskList.get(taskIndex));
                 }
             }
         }
@@ -66,29 +64,40 @@ public class Duke {
         try {
             if (type.equals("todo")) {
                 if (input.isEmpty()) throw new InvalidTaskException("Todo cannot be empty.");
-                taskList[taskCount] = new Todo(input);
-                taskCount++;
+                taskList.add(new Todo(input));
             }
             else if (type.equals("event")){
                 if (!input.contains("/at")) throw new InvalidTaskException("Use the correct command (event DESCRIPTION /at EVENT_DATE).");
                 String[] task = input.split("/at");
                 if (task[0].isEmpty()) throw new InvalidTaskException("Include description (event DESCRIPTION /at EVENT_DATE).");
                 if (task.length != 2) throw new InvalidTaskException("Include description and event date (event DESCRIPTION /at EVENT_DATE).");
-                taskList[taskCount] = new Event(task[0], task[1]);
-                taskCount++;
+                taskList.add(new Event(task[0].trim(), task[1].trim()));
             }
             else if (type.equals("deadline")){
                 if (!input.contains("/by")) throw new InvalidTaskException("Use the correct command (deadline DESCRIPTION /by DEADLINE).");
                 String[] task = input.split("/by");
                 if (task[0].isEmpty()) throw new InvalidTaskException("Include description (deadline DESCRIPTION /by DEADLINE).");
                 if (task.length != 2) throw new InvalidTaskException("Include deadline (deadline DESCRIPTION /by DEADLINE).");
-                taskList[taskCount] = new Event(task[0], task[1]);
-                taskCount++;
+                taskList.add(new Deadline(task[0].trim(), task[1].trim()));
             }
-            System.out.println("added this task: " + taskList[taskCount-1]);
-            System.out.println("you have " + taskCount + " tasks in your list.");
+            System.out.println("added this task: " + taskList.get(taskList.size()-1));
+            System.out.println("you have " + (taskList.size()) + " tasks in your list.");
         }
         catch (InvalidTaskException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void deleteItem(int taskIndex){
+        try {
+            if (taskIndex<1 || taskList.size()<taskIndex) throw new UnknownCommandException("Selected task to delete is invalid.");
+            taskIndex--;
+
+            System.out.println("deleted this task: " + taskList.get(taskIndex));
+            System.out.println("you have " + (taskList.size()-1) + " tasks in your list.");
+            taskList.remove(taskIndex);
+        }
+        catch (UnknownCommandException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -99,7 +108,7 @@ public class Duke {
             bye();
         }
         else if (input.startsWith("list")) {
-            list(taskList, taskCount);
+            list();
         }
         else if (input.startsWith("todo")) {
             addItem("todo", input.replaceFirst("todo", "").trim());
@@ -112,7 +121,13 @@ public class Duke {
         }
         else if (input.startsWith("mark") || input.startsWith("unmark")) {
             String index = input.replaceAll("\\D+","");
+            if (index.isEmpty()) throw new UnknownCommandException("Invalid task input. Use an integer.");
             mark(input, Integer.parseInt(index));
+        }
+        else if (input.startsWith("delete")) {
+            String index = input.replaceAll("\\D+","");
+            if (index.isEmpty()) throw new UnknownCommandException("Invalid task input. Use an integer.");
+            deleteItem(Integer.parseInt(index));
         }
         else {
             throw new UnknownCommandException();

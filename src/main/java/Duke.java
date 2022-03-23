0b81dc9605b1b3e87bedcb202b4beb3ef8dd3dde
hambot072 <97346059+hambot072@users.noreply.github.com>
2022-03-23 22:48:06 +0800

    Level-6
    Add support for deleting tasks from the list and added rudimentary error handling.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.regex.*;
 
 public class Duke {
@@ -9,6 +10,7 @@ public class Duke {
         Pattern todo = Pattern.compile("^todo[\\s][\\s\\S]*$");
         Pattern deadline = Pattern.compile("^deadline[\\s][\\s\\S]*$");
         Pattern event = Pattern.compile("^event[\\s][\\s\\S]*$");
+        Pattern delete = Pattern.compile("^delete[\\s][0-9]*$");
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,29 +20,39 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         String line = "";
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        Task[] temp = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int itemCount = 0;
 
 
         while(!line.contains("bye")) {
             System.out.print("Type something: ");
             line = in.nextLine();
+
             Matcher m = mark.matcher(line);
             Matcher n = unmarked.matcher(line);
             Matcher o = todo.matcher(line);
             Matcher p = deadline.matcher(line);
             Matcher q = event.matcher(line);
+            Matcher r = delete.matcher(line);
 
             if (line.equals("bye")){
                 System.out.print("\tGood day and good bye");
                 break;
             }
 
+            if (line.equals("todo")){
+                System.out.println("The description of a todo task cannot be empty");
+            }
+            if (line.equals("deadline")){
+                System.out.println("The description of a deadline task cannot be empty");
+            }
+            if (line.equals("event")){
+                System.out.println("The description of a event task cannot be empty");
+            }
+
             if(line.equals("list")) {
-                tasks = Arrays.copyOf(temp, itemCount);
-                for (int i = 0; i < tasks.length; i++) {
-                    System.out.print(i+1 + "." + tasks[i].getStatusIcon() + tasks[i] + "\n");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.print(i+1 + "." + tasks.get(i).getStatusIcon() + tasks.get(i) + "\n");
                 }
             }
 
@@ -48,18 +60,18 @@ public class Duke {
 
                 if(m.matches()){
                     line = line.replaceAll("[^0-9]", "");
-                    tasks[Integer.parseInt(line)-1].setStatus(true);
-                    System.out.println("This task has been marked as done: " + "\n" + tasks[Integer.parseInt(line)-1].getStatusIcon() + tasks[Integer.parseInt(line)-1].getDescription());
+                    tasks.get(Integer.parseInt(line)-1).setStatus(true);
+                    System.out.println("This task has been marked as done: " + "\n" + "\t" + tasks.get(Integer.parseInt(line)-1).getStatusIcon() + tasks.get(Integer.parseInt(line)-1));
                 }
                 else if(n.matches()){
                     line = line.replaceAll("[^0-9]", "");
-                    tasks[Integer.parseInt(line)-1].setStatus(false);
-                    System.out.println("This task has been marked as not done: " + "\n" + tasks[Integer.parseInt(line)-1].getStatusIcon() + tasks[Integer.parseInt(line)-1].getDescription());
+                    tasks.get(Integer.parseInt(line)-1).setStatus(false);
+                    System.out.println("This task has been marked as not done: " + "\n" + "\t" + tasks.get(Integer.parseInt(line)-1).getStatusIcon() + tasks.get(Integer.parseInt(line)-1));
                 }
                 else if(o.matches()){
                     line = line.replaceAll("todo ", "");
                     Task t = new Todo(line);
-                    temp[itemCount] = t;
+                    tasks.add(t);
                     itemCount++;
                     System.out.println("This task has been added: " + "\n" + "\t[ ]" + t);
                     System.out.println("There are " + itemCount + " tasks in the list");
@@ -70,7 +82,7 @@ public class Duke {
                     String date = line.substring(line.indexOf("/") + 4, line.lastIndexOf("y"));
                     date = date.concat("y");
                     Task t = new Deadline(name, date);
-                    temp[itemCount] = t;
+                    tasks.add(t);
                     itemCount++;
                     System.out.println("This task has been added: " + "\n" + "\t[ ]" + t);
                     System.out.println("There are " + itemCount + " tasks in the list");
@@ -81,13 +93,20 @@ public class Duke {
                     String date = line.substring(line.indexOf("/") + 4, line.lastIndexOf("m"));
                     date = date.concat("m");
                     Task t = new Event(name, date);
-                    temp[itemCount] = t;
+                    tasks.add(t);
                     itemCount++;
                     System.out.println("This task has been added: " + "\n" + "\t[ ]" + t);
                     System.out.println("There are " + itemCount + " tasks in the list");
                 }
+                else if(r.matches()){
+                    line = line.replaceAll("[^0-9]", "");
+                    itemCount--;
+                    System.out.println("This task has been removed: " + "\n" + "\t" + tasks.get(Integer.parseInt(line)-1).getStatusIcon() + tasks.get(Integer.parseInt(line)-1));
+                    tasks.remove(Integer.parseInt(line)-1);
+                    System.out.println("There are " + itemCount + " tasks in the list");
+                }
                 else {
-                    System.out.println("Regex failed");
+                    System.out.println("Sorry I don't understand what you mean.");
                 }
 
             }

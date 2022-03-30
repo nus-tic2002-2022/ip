ddbcce0754cf51041f9eba5d4353854e50bb8e23
AjazYeap <e0493756@u.nus.edu>
2022-03-31 01:04:04 +0800

    runtest.sh

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -79,12 +79,11 @@ public class Duke {
         }
     }
     public static void main(String[] args) {
-        System.out.println("Hihi\n");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n\n";
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         //System.out.println("Hello! I'm duke.Duke");
         //System.out.println("What can I do for you");

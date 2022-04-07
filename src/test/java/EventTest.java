89a583595b17ce5d8479cdfaab379918bd5c20e7
DESKTOP-5S2J73M\Chay Zhu En <chayzhuen@gmail.com>
2022-04-07 15:37:01 +0800

    #Gradle Support, JDoc and J-unit Testing

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -8,16 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EventTest {
     @Test
     public void EventTest(){
-        Event event = new Event("Event Project Meeting (JUnit)", "23/04/2023 1250");
+        Event event = new Event("Event Project Meeting (TBA2101)", "25/04/2022 1200");
 
         //Create Date and Time Variables
         DateTimeFormatter Standard = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-        String UserInput = event.at;
+        String UserInput = event.event;
         LocalDateTime UserInputDT = LocalDateTime.parse(UserInput, Standard);
 
-        String Format = UserInputDT.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mm"));
+        String Format = UserInputDT.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mma"));
 
-        String EventExpected = "23 Apr 2023 12:50PM";
+        String EventExpected = "25 Apr 2022 12:00PM";
         assertEquals(EventExpected, Format);
     }
 }
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -2,11 +2,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This method takes in 1 parameter and create the task.
+ * @param description Description of the task
+ */
+
+
 public class TodoTest {
     @Test
     public void TodoTest(){
-        Todo todo = new Todo("Todo read book #2 (JUnit)");
-        String TodoExpected = "Todo read book #2 (JUnit)";
+        Todo todo = new Todo("Todo read book (JUnit)","Todo read book (JUnit)");
+        String TodoExpected = "Todo read book (JUnit)";
         assertEquals(TodoExpected, todo.description);
     }
 }

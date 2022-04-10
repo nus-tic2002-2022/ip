29c66d67ace1805582a9bb6ce9ede166b487796a
yutarosensei <kitaharayutaro@gmail.com>
2022-04-10 21:56:14 +0800

    Level 3 - Mark As Done and Undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,33 +3,6 @@ import java.util.Arrays;
 
 public class Duke {
 
-    public static int counter = 0;
-    public static int list_order = 1;
-    public static String[] list = new String[100];
-
-    public static void addlist() {
-        Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
-
-        if (command.toLowerCase().equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        } else if (command.toLowerCase().equals("list")) {
-            String[] print = Arrays.copyOf(list, counter);
-            for (String p : print) {
-                System.out.println(list_order + ". " + p);
-                list_order++;
-            }
-            System.out.println("\n");
-            list_order = 1;
-            addlist();
-        } else {
-            System.out.println("added: " + command + "\n");
-            list[counter] = command;
-            counter++;
-            addlist();
-        }
-
-    }
         public static void main (String[]args){
             String logo = " ____        _        \n"
                     + "|  _ \\ _   _| | _____ \n"
@@ -41,7 +14,10 @@ public class Duke {
             System.out.println("Hello I am Taro the Duke");
             System.out.println("What can I do for you today?");
 
-            addlist();
+            //Task t = new Task("read book");
+            //t.markAsDone();
+            Task.add_list();
+
         }
 
     }

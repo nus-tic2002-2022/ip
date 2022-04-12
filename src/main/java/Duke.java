abb3b0d219c48c59779ed4bdd1dace48c8f14fac
LAPTOP-TCR1IDUO\Kevin Xu Xin <xxydhcg@hotmail.com>
2022-04-13 01:07:38 +0800

    Level-7 All Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.nio.file.Files;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Duke {
+
+    public static ArrayList<Task> taskIcon = new ArrayList<>();
+    public static int count = 0;
 
     // separating line function:
     public static void separatingLine() {
@@ -10,16 +20,6 @@ public class Duke {
 
     public static void system() {
 
-        // codes before level-6 implementation:
-        // String[] taskList = new String[100];
-        // Task[] taskIcon = new Task[100];
-        // String line;
-        // int counter = 0;
-        // Scanner in = new Scanner(System.in);
-
-        // codes from level-6 implementation onwards:
-        // using ArrayList for level-6 implementation:
-        ArrayList<Task> taskIcon = new ArrayList<>();
         String line;
         Scanner in = new Scanner(System.in);
 
@@ -47,12 +47,7 @@ public class Duke {
                     // below codes are used from level-6 onwards:
                     System.out.println(i + 1 + ". " + taskIcon.get(i).toString());
 
-                    // below codes are used before level-4 implementation:
-                    //System.out.println(i + 1 + ". " +
-                    //        "[" +
-                    //        taskIcon[i].getStatusIcon() +
-                    //        "] " +
-                    //        taskList[i]);
+
                 }
                 separatingLine();
 
@@ -313,29 +308,87 @@ public class Duke {
 
             }
 
-            //below codes are implemented before level-5:
+
             //Add user input to task list:
-            // else {
-            //  separatingLine();
-            //  System.out.println("The below task has been added to your task list:\n " +
-            //          line);
+            else {
+              separatingLine();
+              System.out.println("Opps! Sorry, BOSS! But I do not know what it means!\n" +
+                      "Please enter the valid task description with pre-fix as below:\n" +
+                      "'todo' or 'deadline' or 'event' or 'delete'\n" +
+                      "Please kindly try again with the above pre-fix, Boss!");
+              separatingLine();
 
-                //assign to task list from input for printing out task description when request for printing lists:
-                //taskList[counter] = line;
 
-                //Add to task for separate icon tracking and print status mark:
-                //Task t = new Task(line);
-                //t.description = line;
-                //taskIcon[counter] = t;
-
-                //update counter to show sequence numbering when print result:
-                //separatingLine();
-                //counter = counter + 1;
-            //}
+            }
 
         }
 
     }
+
+    //Save the File to Data Folder
+    public static void saveFile(String myOutput) throws IOException {
+        FileWriter fw = new FileWriter(myOutput);
+        for (Task t : taskIcon){
+            fw.write(t + System.lineSeparator());
+        }
+        fw.close();
+    }
+
+
+    //Read the File from Data Folder
+    public static void readFile(String myOutput) throws FileNotFoundException {
+        File f = new File(myOutput);
+        Scanner s = new Scanner(f);
+
+        while (s.hasNext()) {
+            String text = s.nextLine();
+            //Read [T] from File and Update to ArrayList
+            if (text.contains("[T]")) {
+                String Todo = text.substring(23);
+                taskIcon.add(new Todo(Todo));
+
+                if (text.contains("[X]")) {
+                    Task m = taskIcon.get(count);
+                    m.markAsDone();
+                }
+                count++;
+
+                //Read [D] from File and Update to ArrayList
+            } else if (text.contains("[D]")) {
+
+                //Find '('
+                int by_pos = text.indexOf("(");
+                String Deadline = text.substring(23, by_pos-1);
+                String by = text.substring(by_pos + 5, by_pos + 24);
+
+                taskIcon.add(new Deadline(Deadline, by));
+
+                if (text.contains("[X]")) {
+                    Task m = taskIcon.get(count);
+                    m.markAsDone();
+                }
+                count++;
+
+                //Read [E] from File and Update to ArrayList
+            } else if (text.contains("[E]")) {
+
+                //Find '('
+                int at_pos = text.indexOf("(");
+
+                String Event = text.substring(23, at_pos-1);
+                String at = text.substring(at_pos + 5, at_pos + 24);
+
+                taskIcon.add(new Event(Event, at));
+
+                if (text.contains("[X]")) {
+                    Task m = taskIcon.get(count);
+                    m.markAsDone();
+                }
+                count++;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -350,6 +403,19 @@ public class Duke {
                 "What can I do to assist you today, Boss?\n" +
                 "(Please input your task or type 'list' to retrieve your task list)");
         separatingLine();
+
+
+        //Read File
+        String FileLocation = "outputFiles/duke.txt";
+        String Directory = "./outputFiles/";
+        try {
+            Path path = Paths.get(Directory);
+            Files.createDirectories(path);
+            readFile(FileLocation);
+        } catch (IOException e) {
+            System.out.println("OOPS!!! I'm sorry, Boss! I can't find the file!");
+        }
+
 
         // perform system logic
         system();

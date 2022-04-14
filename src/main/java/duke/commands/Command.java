b5ea04cba70f9b597a89e28e393dbc9abb9cc162
Winson Mok <e0493706@u.nus.edu>
2022-04-14 16:19:01 +0800

    A-CodeQuality
    Modify to improve code quality :
    - Avoid Complicated Expressions
    - Make code obvious

--- src/main/java/duke/commands/Command.java
+++ src/main/java/duke/commands/Command.java
@@ -69,74 +69,76 @@ public class Command {
      * @return Information on executed actions (successful or unsuccessful) will be returned as ResultCommand.
      */
     public ResultCommand execute() throws DukeException, IOException {
-        if (this.userInput.getUserInput(0).equalsIgnoreCase("todo")) {
-            Task new_task = AddCommand.addTodo(this.userInput, this.taskList);
-            return new ResultCommand("addTask", new_task);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("deadline")) {
-            Task new_task = AddCommand.addDeadline(this.userInput, this.taskList);
-            return new ResultCommand("addTask", new_task);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("event")) {
-            Task new_task = AddCommand.addEvent(this.userInput, this.taskList);
-            return new ResultCommand("addTask", new_task);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("echo")) {
-            String content = "";
-            for (int i = 1; i < userInput.getUserInputSize(); i++) {
-                content = content + " " + userInput.getUserInput(i);
-            }
-            return new ResultCommand("echo", content);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("bye") || this.userInput.getUserInput(0).equalsIgnoreCase("exit")) {
-            return new ResultCommand("goodbye", "");
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("hello")) {
-            return new ResultCommand("hello", "");
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("list")) {
-            ListCommand.showList(taskList);
-            return new ResultCommand("noReply", "");
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("delete")) {
-            DeleteCommand.deleteTask(this.userInput.getUserInput(1), taskList);
-            return new ResultCommand("delete", "");
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("mark")) {
-            boolean change = MarkCommand.markTask(this.userInput.getUserInput(1), taskList);
-            if (change) {
-                return new ResultCommand("updateList", "");
-            }
-            else {
-                return new ResultCommand("markNoChange", "");
-            }
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("unmark")) {
-            boolean change = UnmarkCommand.unmarkTask(this.userInput.getUserInput(1), taskList);
-            if (change) {
-                return new ResultCommand("updateList", "");
-            }
-            else {
-                return new ResultCommand("unmarkNoChange", "");
-            }
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("save")) {
-            return new ResultCommand("save", "");
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("free")) {
-            String message = FindCommand.findFreeTime(taskList);
-            return new ResultCommand("freeTime", message);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("find")) {
-            TaskList found = FindCommand.findTask(this.userInput, taskList);
-            return new ResultCommand("find", found);
-        }
-        else if (this.userInput.getUserInput(0).equalsIgnoreCase("update")) {
-            Task updatedTask = UpdateCommand.updateDescription(this.userInput, taskList);
-            return new ResultCommand("updateTask", updatedTask);
-        }
-        else {
-            return new ResultCommand("?", "");
+        String keyword = this.userInput.getUserInput(0);
+        switch (keyword) {
+            case "todo":
+                Task newTodoTask = AddCommand.addTodo(this.userInput, this.taskList);
+                return new ResultCommand("addTask", newTodoTask);
+
+            case "deadline":
+                Task newDeadlineTask = AddCommand.addDeadline(this.userInput, this.taskList);
+                return new ResultCommand("addTask", newDeadlineTask);
+
+            case "event":
+                Task newEventTask = AddCommand.addEvent(this.userInput, this.taskList);
+                return new ResultCommand("addTask", newEventTask);
+
+            case "echo":
+                String content = "";
+                for (int i = 1; i < userInput.getUserInputSize(); i++) {
+                    content = content + " " + userInput.getUserInput(i);
+                }
+                return new ResultCommand("echo", content);
+
+            case "bye":
+
+            case "exit":
+                return new ResultCommand("goodbye", "");
+
+            case "hello":
+                return new ResultCommand("hello", "");
+
+            case "list":
+                ListCommand.showList(taskList);
+                return new ResultCommand("noReply", "");
+
+            case "delete":
+                DeleteCommand.deleteTask(this.userInput.getUserInput(1), taskList);
+                return new ResultCommand("delete", "");
+
+            case "mark":
+                boolean isMarked = MarkCommand.markTask(this.userInput.getUserInput(1), taskList);
+                if (isMarked) {
+                    return new ResultCommand("updateList", "");
+                } else {
+                    return new ResultCommand("markNoChange", "");
+                }
+
+            case "unmark":
+                boolean isUnmarked = UnmarkCommand.unmarkTask(this.userInput.getUserInput(1), taskList);
+                if (isUnmarked) {
+                    return new ResultCommand("updateList", "");
+                } else {
+                    return new ResultCommand("unmarkNoChange", "");
+                }
+
+            case "save":
+                return new ResultCommand("save", "");
+
+            case "free":
+                String message = FindCommand.findFreeTime(taskList);
+                return new ResultCommand("freeTime", message);
+
+            case "find":
+                TaskList found = FindCommand.findTask(this.userInput, taskList);
+                return new ResultCommand("find", found);
+
+            case "update":
+                Task updatedTask = UpdateCommand.updateDescription(this.userInput, taskList);
+                return new ResultCommand("updateTask", updatedTask);
+
+            default:
+                return new ResultCommand("?", "");
         }
     }
 }

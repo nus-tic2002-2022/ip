270f8de89d12bec9515ec587b727b23b2d8a1b06
acidwuf <acidwuf@gmail.com>
2022-04-15 18:43:07 +0800

    Level 3. Mark as Done. Changed from ArrayList to Array. couldn't get arraylist to work. Added Task Class. Changed from switch to if else due to needing to read the number in the input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,58 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Duke {
-    public static void showTaskList(ArrayList<String> taskList){
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+        public void setIsDone() {
+            this.isDone = true;
+        }
+        public void setNotDone() {
+            this.isDone = false;
+        }
+        public void showTaskStatus() {
+            System.out.println("        " + "[" + getStatusIcon() + "] " + getDescription());
+        }
+    }
+    public static void showTaskList(Task[] taskList, int taskCounter){
         int counter = 1;
-
-        for (int i = 0; i < taskList.size(); i++) {
-
-            System.out.println("    " + counter + ". " + taskList.get(i));
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println("    " + counter + ".[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
             counter++;
         }
     }
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);
+         */
+        Task[] taskList = new Task[100];
         String userInput = "notBye";
-        ArrayList<String> taskList = new ArrayList<>();
+        int taskCounter = 0;
+        //ArrayList<Task> taskList = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
         System.out.println("    ____________________________________________________________");
@@ -29,17 +62,38 @@ public class Duke {
 
         do {
             userInput = in.nextLine();
-            System.out.println("    ____________________________________________________________");
-
-            switch (userInput) {
-                case "list": showTaskList(taskList);
-                    break;
-
-                default: taskList.add(userInput);
-                    System.out.println("    added: " + userInput); //do stuff here after input
-                    break;
+            if(userInput.startsWith("mark")){
+                taskList[Integer.parseInt(userInput.substring(5))-1].setIsDone();
+                System.out.println("    Job's done:");
+                taskList[Integer.parseInt(userInput.substring(5))-1].showTaskStatus();
+            } else if(userInput.startsWith("unmark")){
+                taskList[Integer.parseInt(userInput.substring(7))-1].setNotDone();
+                System.out.println("    Job IS NOT done:");
+                taskList[Integer.parseInt(userInput.substring(7))-1].showTaskStatus();
+            } else if(userInput.startsWith("list")){
+                showTaskList(taskList, taskCounter);
+            } else {
+                taskList[taskCounter] = new Task(userInput);
+                System.out.println("    added: " + userInput); //do stuff here after input
+                taskCounter++;
             }
+
             System.out.println("    ____________________________________________________________");
+
+ /*           switch (userInput) {
+                case "list": showTaskList(taskList, taskCounter);
+                    break;
+                case "mark": userInput
+                    break;
+
+                default: taskList[taskCounter] = new Task(userInput);
+                    System.out.println("    added: " + userInput); //do stuff here after input
+                    taskCounter++;
+                    break;
+            }*/
+            System.out.println("    ____________________________________________________________");
+
+
         } while (!userInput.equals("bye"));
 
 

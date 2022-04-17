e0e88a2b15f01209d9a4112ff0d13444f9cd9a17
CyberS-N <e0493676@u.nus.edu>
2022-04-17 19:45:19 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,47 +6,65 @@ public class Duke {
         public static int seq = 1;
     public static Task[] list = new Task[100];
 
-    public static void sort(String t) {
+    public static void sort(String t) throws DukeException {
         if (t.contains("todo")) {
+            if (t.trim().length() < 5) {
+                throw new DukeException("OOPS!!! Description of task cannot be empty.\n");
+            }
+
             String description = t.substring(5);
-            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            Task[] print = Arrays.copyOf(list,count);
             for (Task p : print) {
                 if (p.description.equals(description)) {
-                    System.out.println("Error: Task has already been added previously\n");
-                    echo();
+                    throw new DukeException("OOPS!!! Task has already been added previously\n");
                 }
             }
             list[count] = new Todo(description);
         }
         else if (t.contains("deadline")) {
+            if (t.trim().length() < 9) {
+                throw new DukeException("OOPS!!! Description of task cannot be empty.\n");
+            }
+            if (!t.contains("/")) {
+                throw new DukeException("OOPS!!! Please specify time.\n");
+            }
+
             int n = t.indexOf('/');
             String description = t.substring(9, n-1);
             String by = t.substring(n+4);
-            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            Task[] print = Arrays.copyOf(list,count);
             for (Task p : print) {
                 if (p.description.equals(description)) {
-                    System.out.println("Error: Task has already been added previously\n");
-                    echo();
+                    throw new DukeException("OOPS!!! Task has already been added previously\n");
                 }
             }
             list[count] = new Deadline(description, by);
         }
         else if (t.contains("event")) {
+            if (t.trim().length() < 6) {
+                throw new DukeException("OOPS!!! Description of task cannot be empty.\n");
+            }
+            if (!t.contains("/")) {
+                throw new DukeException("OOPS!!! Please specify time.\n");
+            }
+
             int n = t.indexOf('/');
             String description = t.substring(6, n-1);
             String at = t.substring(n+4);
-            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            Task[] print = Arrays.copyOf(list,count);
             for (Task p : print) {
                 if (p.description.equals(description)) {
-                    System.out.println("Error: Task has already been added previously\n");
-                    echo();
+                    throw new DukeException("OOPS!!! Task has already been added previously\n");
                 }
             }
             list[count] = new Event(description, at);
         }
+        else {
+            throw new DukeException("OOPS!!! Please enter a valid task such as todo / deadline / event\n");
+        }
     }
 
-    public static void echo() {
+    public static void echo() throws DukeException {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
@@ -56,7 +74,7 @@ public class Duke {
             System.exit(0);
         }
         else if (line.equals("list")) {
-            System.out.println("Here are the tasks in your list:\n");
+            System.out.println("Here are the tasks in your list:");
             Task[] print = Arrays.copyOf(list,count);
             for (Task p : print) {
                 System.out.println(seq + ". " + p);
@@ -64,43 +82,37 @@ public class Duke {
             }
             System.out.println("\n");
             seq = 1;
-            echo();
         }
-        else if (line.contains("done")) {
+        else if (line.contains("mark")) {
             String[] words = line.split(" ");
-            if (words.length < 2) {
-                System.out.println("Error: Please enter which task is done\n");
-                echo();
+            if (words.length < 2 || words[1].trim().equals("")) {
+                throw new DukeException("OOPS!!! Please enter which task is done\n");
             }
             int n = Integer.parseInt(words[1]);
             if (n > count) {
-                System.out.println("Error: Please enter a valid task number\n");
+                System.out.println("OOPS!!! Please enter a valid task number\n");
                 echo();
             }
             if (list[n-1].getStatusIcon().equals("\u2713")) {
-                System.out.println("Error: Task has already been completed");
-                System.out.println(list[n-1] + "\n");
-                echo();
+                throw new DukeException("OOPS!!! Task has already been completed");
             }
             else {
-                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("Good Job! I've marked this task as done:");
                 list[n-1].setStatus(true);
                 System.out.println(list[n-1] + "\n");
-                echo();
             }
         }
         else {
             sort(line);
-            System.out.println("Got it. I've added this task:");
+            System.out.println("Done. I've added this task:");
             System.out.println(list[count]);
             count++;
-            System.out.println("Now you have " + count + " tasks in the list.\n");
-            echo();
+            System.out.println("Total " + count + " tasks in the list.\n");
         }
     }
 
 
-        public static void main (String[]args){
+        public static void main (String[]args) throws DukeException {
 
             String logo = " ____        _        \n"
                     + "|  _ \\ _   _| | _____ \n"
@@ -111,6 +123,12 @@ public class Duke {
 
             String hello = "Hi! This Duke :)\n" + "What can I do for you?\n";
             System.out.println(hello);
-            echo();
+            while(true) {
+                try {
+                    echo();
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
     }

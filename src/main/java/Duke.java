850dab170fe4703d8d7fb1c21daaf14a7c8df4ed
CyberS-N <e0493676@u.nus.edu>
2022-04-17 19:06:30 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,31 +4,58 @@ import java.util.Arrays;
 public class Duke {
         public static int count = 0;
         public static int seq = 1;
-        public static String[] list = new String[100];
+    public static Task[] list = new Task[100];
 
-        public static void echo () {
-            String line;
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
 
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-            } else if (line.equals("list")) {
-                String[] print = Arrays.copyOf(list, count);
-                for (String p : print) {
-                    System.out.println(seq + ". " + p);
-                    seq++;
-                }
-                System.out.println("\n");
-                seq = 1;
+        if (line.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else if (line.equals("list")) {
+            System.out.println("Here are the tasks in your list:\n");
+            Task[] print = Arrays.copyOf(list,count);
+            for (Task p : print) {
+                System.out.println(seq + ". " + p);
+                seq++;
+            }
+            System.out.println("\n");
+            seq = 1;
+            echo();
+        }
+        else if (line.contains("mark")) {
+            String[] words = line.split(" ");
+            if (words.length < 2) {
+                System.out.println("Error: Please enter which task is done\n");
                 echo();
-            } else {
-                System.out.println("added: " + line + "\n");
-                list[count] = line;
-                count++;
+            }
+            int n = Integer.parseInt(words[1]);
+            if (n > count) {
+                System.out.println("Error: Please enter a valid task number\n");
+                echo();
+            }
+            if (list[n-1].getStatusIcon().equals("\u2713")) {
+                System.out.println("Error: Task has already been completed");
+                System.out.println(list[n-1] + "\n");
+                echo();
+            }
+
+            else {
+                System.out.println("Nice! I've marked this task as done:");
+                list[n-1].setStatus(true);
+                System.out.println(list[n-1] + "\n");
                 echo();
             }
         }
+        else {
+            System.out.println("added: " + line + "\n");
+            list[count] = new Task(line);
+            count++;
+            echo();
+        }
+    }
 
 
         public static void main (String[]args){
